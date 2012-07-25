4250483c3b7a05b4941e825b7fe265c3aea03426
shivangi <dell@141.140.157.138>
2012-07-25 15:59:07 -0500

    okay

--- helloworld/src/hello.java
+++ helloworld/src/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String args[]){
 		System.out.println("What's up world");
 		System.out.println("First test completed.");
+		System.out.println("This is good. Carry on with your projects.");
 	}
 	
 }
